560874a5aef87ef24557404a58db4d8826325dcb
KeerthanaChenna13 <keerthanachenna13@gmail.com>
2026-09-17 21:36:34 +0530

    Update app(1).py

--- app(1).py
+++ app(1).py
@@ -4,8 +4,6 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import streamlit as st
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 
 # =========================================================
